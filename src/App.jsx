39a4fe282392b64c62d556fe445c2eb9571157bb
Buddynice34 <willemapril33@gmail.com>
2024-07-12 00:13:56 +0200

    Created a simple routing temlate

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
 import { useAuth } from './context/AuthContext';
+
 
 function App() {
 
+  const { currentUser } = useAuth();
+
   return (
     <>
-      <Container className='' style={{minHeight: "100vh"}}>
-        <div className="d-flex align-items-center justify-content-center" style={{maxWidth: "400px", background: "red"}}>
-          <Card>
-            <Card.Header as="h5">Featured</Card.Header>
-            <Card.Body>
-              <Card.Title>Special title treatment</Card.Title>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
+      <Container className='' style={{ minHeight: "100vh" }}>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<h1>Home</h1>} />
+            <Route path='/signup' element={<h1>SignUp</h1>} />
+            <Route path='/login' element={<h1>SignUp</h1>} />
+            {currentUser && <Route path='/profile' element={<h1>SignUp</h1>} />
+            }
+            {currentUser && <Route path='/main' element={<h1>Main</h1>} />
+            }
+            <Route path='*' element={<h1>Home</h1>} />
+          </Routes>
+        </Router>
       </Container>
     </>
   )
