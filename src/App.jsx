60c92cbb793c47ae370343ca19c5bb48f571bcbd
Buddynice34 <willemapril33@gmail.com>
2024-07-11 23:07:35 +0200

    Bootstrap template

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className='d-flex align-items-center justify-content-center' style={{minHeight:"100vh"}}>
+        <div className="" style={{maxWidth: "400px", background: "red"}}>
+          <Card>
+            <Card.Header as="h5">Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant='secondary'>Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
     </>
   )
 }
